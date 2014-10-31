a0bc2c1fdc15d2266cc49dba37795377bf4b89cb
evan5090 <evan5090@ISS147D8-19.scdsb.pri>
2014-10-31 09:14:53 -0400

    ayy lmao

--- src/guessinggame/GuessMachine.java
+++ src/guessinggame/GuessMachine.java
@@ -1,47 +1,49 @@
 package guessinggame;
 
-
 public class GuessMachine {
+
     private int num;
     private int numguesses;
     private int guess;
     private String giveHint;
     private boolean setGuess;
-            
-    public GuessMachine(){
-        num=(int)(Math.random()*100)+1;
-        guess=0;
-        numguesses=0;
+
+    public GuessMachine() {
+        num = (int) (Math.random() * 100) + 1;
+        guess = 0;
+        numguesses = 0;
     }
-    
-    public String giveHint(){
-        if(guess == num){
-            giveHint =("You got it!");
-              
+
+    public String giveHint() {
+        if (guess == num) {
+            giveHint = ("You got it!");
+
         } else if (guess > num) {
-            giveHint= ("Guess lower");
-           
+            giveHint = ("Guess lower");
+
         } else if (guess < num) {
-            giveHint=("Guess higher");
-            
+            giveHint = ("Guess higher");
+
         }
         return giveHint;
     }
-    
-    public boolean setGuess(int number){
-        if (number <=100 || number >0){
-            number=guess;
+
+    public boolean setGuess(int number) {
+        if (number <= 100 && number > 0) {
+            guess = number;
+            numguesses++;
             return true;
+
+        } else {
+            return false;
         }
-        else return false;
-        
+
     }
     
-    
-    
-    
-    
-    
+    public int getNumGuesses(){
+        return numguesses;
+    }
+
     //to do - add methods
     //giveInt(), setGuess(), getNumGuesses()
 }
